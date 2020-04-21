0c1495548b56471b4eae0c04759e72b4a83c8a4c
briannalturner <blturner3527@gmail.com>
2020-04-21 16:01:33 -0400

    created users table, installing active storage

---
+++ db/migrate/20200421195331_create_users.rb
@@ -0,0 +1,21 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.text :bio
+      t.integer :year
+      t.string :gender
+      t.string :orientation
+      t.date :birthdate
+      t.boolean :likes_women
+      t.boolean :likes_men
+      t.boolean :likes_other
+      t.boolean :asexual
+      t.string :house
+      t.string :favorite_spell
+
+      t.timestamps
+    end
+  end
+end
